e249910b346a998a6d26d3686d01a69f0df8f3fe
RossiVienas <Dissmind@gmail.com>
2020-02-02 02:33:41 +0200

    [ADD] in lab3 all algorithms (exception for 12, 13 ,17, 18, 19, 21, 23, 24, 30) [REFACTORING] lab3

--- OldLabs/Lab3.cs
+++ OldLabs/Lab3.cs
@@ -11,15 +11,15 @@ namespace Labs.OldLabs
         public static string Calculate(string strArray)
         {
             // Conversion string in int array
-            int[] array = ConversionArray(strArray);
+            double[] array = ConversionArray(strArray);
 
             // Algorithm calculation
-            int result = algorithms(array);
+            double result = Algorithms(array);
 
             return result.ToString();
         }
 
-        public static int[] ConversionArray(string str)
+        private static double[] ConversionArray(string str)
         {
             // Counting size array
             int arrayLength = 0;
@@ -29,7 +29,7 @@ namespace Labs.OldLabs
                 if (str[i] == ' ') arrayLength++;
             }
 
-            int[] array = new int[arrayLength + 1];
+            double[] array = new double[arrayLength + 1];
 
 
             // Array filling
@@ -50,21 +50,102 @@ namespace Labs.OldLabs
             return array;
         }
 
-        public static int algorithms(int[] array)
+        public static double Algorithms(double[] array)
         {
-            int result = 0;
+            double result = 0;
 
-            switch (Configuration.Variant)
+            // Array variant for multiple
+            int[] MultiVariant = { 2, 3, 5, 7, 11, 13, 15, 17, 19, 20, 22, 26, 28, 30 };
+
+            bool isMult = false;
+
+            foreach (int i in MultiVariant) 
+                if (i == int.Parse(Configuration.Variant)) isMult = true;
+
+            foreach (double i in array)
             {
-                case "1":
-                    foreach (int i in array)
+                if (ConditionAlgorithms(i))
+                {
+                    if (isMult)
                     {
-                        if (i % 3 == 0) result += i;
+                        if (result == 0) result++;
+                        result *= i;
                     }
-                    break;
+                    else result += i;
+                }
             }
 
             return result;
+        }
+
+        private static bool ConditionAlgorithms(double i)
+        {
+            // Array condition algorithms for variant
+            bool[] conditionAlgorithms =
+            {
+                // 1
+                i % 3 == 0,
+                // 2
+                i % 5 == 0,
+                // 3
+                i % 10 == 2,
+                // 4
+                i % 10 != 1,
+                // 5
+                i % 2 != 0 && i < 0,
+                // 6
+                i % 2 == 0 && i > 0,
+                // 7
+                i < 10 && i > -10,
+                // 8
+                i % 7 != 0, 
+                // 9
+                i % 10 == 0,
+                // 10
+                i < 70 && i > -70,
+                // 11
+                i < 50 && i > -50,
+                // 12
+                // TODO: Сумма максимального и минимального числа
+                // 13
+                // TODO: Числа с нулевой дробной частью  
+                // 14
+                i > 0 && i % 3 == 0,
+                // 15
+                i < 0 && i % 8 == 0,
+                // 16
+                i % 2 != 0 && i % 11 != 0,
+                // 17
+                // TODO: Нечетная целая часть
+                // 18
+                // TODO: Четная целая часть
+                // 19
+                // TODO: Абсолютная величина которых не превосходит 10
+                // 20
+                i % 3 == 0 && i % 5 != 0,
+                // 21
+                // TODO: Целая часть, которая не делится 3
+                // 22
+                (i % 10) % 2 == 0,
+                // 23
+                // TODO: C ненулевой дробной частью
+                // 24
+                // TODO: Абсолютная величина которых больше 30
+                // 25
+                i % 5 == 0 && i % 3 == 0,
+                // 26
+                i % 6 == 0,
+                // 27
+                i % 10 == 4,
+                // 28
+                i % 10 != 1 && i % 10 != 2,
+                // 29
+                i < 0,
+                // 30
+                // TODO: не превосходящих 20 по абсолютной величине
+            };
+
+            return conditionAlgorithms[int.Parse(Configuration.Variant) - 1];
         }
 
     }
